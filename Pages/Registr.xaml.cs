2bc7295e79c23970c8f2913a76ed275aa4780f4d
csamk <csamk@SUMMRSXO>
2025-01-09 21:26:17 +0500

    реализована страница регистрации

--- Pages/Registr.xaml.cs
+++ Pages/Registr.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,50 @@ namespace zdvokzal.Pages
     /// </summary>
     public partial class Registr : Page
     {
+        public static Pod_Registr pod;
         public Registr()
         {
             InitializeComponent();
+            pod = new Pod_Registr();
+        }
+        public void OpenPagePod()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame_first.Navigate(pod);
+                DoubleAnimation opgrisAnimation = new DoubleAnimation();
+                opgrisAnimation.From = 0;
+                opgrisAnimation.To = 1;
+                opgrisAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame_first.BeginAnimation(Frame.OpacityProperty, opgrisAnimation);
+            };
+            frame_first.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
+        }
+        private void Click_Registr(object sender, RoutedEventArgs e)
+        {
+
+            if (password.Password != password_povt.Password)
+            {
+                MessageBox.Show("Пароли не совпадают");
+                return;
+            }
+            if (string.IsNullOrEmpty(login.Text))
+            {
+                MessageBox.Show("Введите логин");
+                return;
+            }
+            MessageBox.Show("пользователя должен подтвердить администратор");
+            MainWindow.init.OpenPageFirst();
+            login.Text = "";
+        }
+        private void Click_Vhod(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.init.OpenPageFirst();
         }
     }
 }
